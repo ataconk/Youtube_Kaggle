48b6ae647125f67f9a9e133347be9adf425a3f20
atacan korkmaz <atacankorkmaz3@gmail.com>
2018-08-13 09:37:01 +0200

    081318

--- docOps/file_reader.py
+++ docOps/file_reader.py
@@ -9,6 +9,8 @@ class documentHandler:
     def __init__(self):
         pass
 
+
+
     def get_csv(self, path):
         self.reader = pd.read_csv(path)
         return self.reader
@@ -18,11 +20,15 @@ class documentHandler:
             file = json.load(self.reader)
             return file
 
+    def get_tsv(self, path):
+        self.reader = pd.read_csv(path, delimiter ="\t")
+        return self.reader
+
     def json_to_df(self, normalizer, path):
 
         # with open(path) as self.reader:
         #     file = json.load(self.reader)
-         # self.get_json(path)
+        # self.get_json(path)
 
         data_frame = json_normalize(self.get_json(path)[normalizer])
         return data_frame
@@ -39,13 +45,15 @@ class documentHandler:
                 for subsubelem in subelem:
                     print('         ', subsubelem.tag, ': ', subsubelem.text)
 
+    def get_text(self, path):
+        f = open(path)
+        yourList = f.readlines()
+        return yourList
 
-dh = documentHandler()
-ax = dh.get_json('data\\DE_category_id.json')
-axx = dh.json_to_df('items', 'data\\DE_category_id.json')
-axxx = dh.xml_reader('C:\\workspace\\config.xml')
 
-#
+# dh = documentHandler()
+# ax = dh.get_json('data\\DE_category_id.json')
+# axx = dh.json_to_df('items', 'data\\DE_category_id.json')
+# axxx = dh.xml_reader('C:\\workspace\\config.xml')
+
 # axxx.show(limit=10)
-
-print(axx)
